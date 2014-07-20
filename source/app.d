26a6fd803a03133ce7afdeb86bf7eba6833e8ad7
Bob Tolbert <bob@tolbert.org>
2014-07-20 14:55:59 -0600

    Cleaned up interface.
    Added -n for no recurse.
    
    Added actual file extensions to --help-types.
    
    Added some exception handling for bad file/directory name on
    command line.

--- source/app.d
+++ source/app.d
@@ -18,12 +18,12 @@ import std.cstream;
 
 import docopt;
 
-public bool extMatch(DirEntry de, int[string] exts)
+public bool extMatch(const DirEntry de, const int[string] exts)
 {
     return exts.get(extension(de.name), 0) == 1;
 }
 
-public bool nameMatch(DirEntry de, int[string] names)
+public bool nameMatch(const DirEntry de, const int[string] names)
 {
     return names.get(baseName(de.name), 0) == 1;
 }
@@ -39,11 +39,14 @@ void addCFiles(ref int[string] exts) {
 
 void addCPPFiles(ref int[string] exts) {
     exts[".h"] = 1;
+    exts[".hh"] = 1;
     exts[".cc"] = 1;
     exts[".cpp"] = 1;
     exts[".cxx"] = 1;
     exts[".c++"] = 1;
     exts[".hpp"] = 1;
+    exts[".hxx"] = 1;
+    exts[".tpp"] = 1;
 }
 
 void addCSharpFiles(ref int[string] exts) {
@@ -72,8 +75,8 @@ void getDefaultExtensions(ref int[string] exts, ref int[string] names) {
     addPyFiles(exts);
 }
 
-void searchOneFileStream(T)(InputStream inp, string filename,
-                            Regex!T matcher, docopt.ArgValue[string] arguments)
+void searchOneFileStream(T)(InputStream inp, const string filename,
+                            Regex!T matcher, const docopt.ArgValue[string] arguments)
 {
     bool first = true;
     bool reverse = arguments["-v"].isTrue();
@@ -102,41 +105,55 @@ void searchOneFileStream(T)(InputStream inp, string filename,
 
 int main(string[] args)
 {
-
-    auto doc = "
+    auto usage = "
 Usage: sift [options] PATTERN [FILES ...]
        sift -f [options] [FILES ...]
+       sift --help
+       sift --help-types
+       sift --version
+    ";
 
+    auto doc = "
 Arguments:
     PATTERN     pattern to search for
     FILES       files or directories to search. [default: .]
 
-Options:
+Search options:
     -v                     Reverse the match.
     -Q --literal           Quote all meta-characters.
     -i --case-insensitive  Case-insensitive match.
+
+Output options:
     -H --with-filename     Include filename before match.
     -h --no-filename       No filename before match.       
     --no-color             no color output
-    --name-only            Show only filename of matches
-    --depth-first          Depth first search
+    -g, --name-only        Show only filename of matches
 
 Base options:
     --help
+    --help-types           Show help on file type flags.
     --version              Show version and exit.
 
 File find options:
-    -f             Only print files selected.
-    --sort-files   Sort the files found.
+    -f                     Only print files selected.
+    --sort-files           Sort the files found.
 
+File inclusion options:
+    -n, --no-recurse       No descending into subdirectories
+    --follow               Follow symlinks.  Default is off.
+
+    ";
+
+    auto typeOptions = "
 File type options:
-    --d         D files
-    --c         C/C++ files
-    --c++       C/C++ files
-    --cmake     CMake files
-    --csharp    C# files
-    --hy        Hy files
-    --py        Python files
+    --d         D files        [.d]
+    --c         C files        [.c .h]
+    --cpp       C++ files      [.cpp .cc .cxx .c++ .hpp .tpp .hh .h .hxx]
+    --clojure   Clojure files  [.clj]
+    --cmake     CMake files    [CMakeLists.txt .cmake]
+    --csharp    C# files       [.cs]
+    --hy        Hy files       [.hy]
+    --py        Python files   [.py]
     ";
 
     string[dchar] metaTable = ['[': "\\[",
@@ -152,14 +169,25 @@ File type options:
                                '\\': "\\\\",
                                '.': "\\."];
 
-    auto arguments = docopt.docopt(doc, args[1..$], true, "0.1.0");
+    auto miniDoc = usage ~ doc;
+    auto arguments = docopt.docopt(miniDoc, args[1..$], true, "0.1.0");
+
+    if (arguments["--help-types"].isTrue()) {
+        write(usage);
+        writeln(typeOptions);
+        return 0;
+    }
+
+    auto fullDoc = miniDoc ~ typeOptions;
+    arguments = docopt.docopt(fullDoc, args[1..$], true);
 
     //writeln(arguments);
 
     auto spanMode = SpanMode.breadth;
-    if (arguments["--depth-first"].isTrue()) {
-        spanMode = SpanMode.depth;
+    if (arguments["--no-recurse"].isTrue()) {
+        spanMode = SpanMode.shallow;
     }
+    bool follow = arguments["--follow"].isTrue();
 
     auto flags = "";
     if (arguments["--case-insensitive"].isTrue()) {
@@ -182,7 +210,7 @@ File type options:
     if (arguments["--c"].isTrue()) {
         addCFiles(userExts);
     }
-    if (arguments["--c++"].isTrue()) {
+    if (arguments["--cpp"].isTrue()) {
         addCFiles(userExts);
         addCPPFiles(userExts);
     }
@@ -206,34 +234,44 @@ File type options:
 //    writeln(arguments["FILES"]);
 
     auto FILES = arguments["FILES"].asList();
-    if (FILES.length == 1 && FILES[0] == "-") {
-        arguments["--no-filename"] = new docopt.ArgValue(true);
-    } else if (FILES.length == 1 && FILES[0].isFile && arguments["--with-filename"].isFalse()) {
-        arguments["--no-filename"] = new docopt.ArgValue(true);
-    } else if (arguments["--with-filename"].isTrue()) {
-        arguments["--no-filename"] = new docopt.ArgValue(false);
+    try {
+        if (FILES.length == 1 && FILES[0] == "-") {
+            arguments["--no-filename"] = new docopt.ArgValue(true);
+        } else if (FILES.length == 1 && FILES[0].isFile && arguments["--with-filename"].isFalse()) {
+            arguments["--no-filename"] = new docopt.ArgValue(true);
+        } else if (arguments["--with-filename"].isTrue()) {
+            arguments["--no-filename"] = new docopt.ArgValue(false);
+        }
+    } catch(std.file.FileException e) {
+        writeln("Unknown file: ", FILES[0]);
+        return -1;
     }
 
     string [] fileList;
     foreach(item; FILES) {
-        if (item == "-") {
-            fileList ~= item;
-        } else if (item.isFile) {
-            fileList ~= item;
-        } else if (item.isDir) {
-            auto dirName = buildNormalizedPath(item);
-            auto files = dirEntries(dirName, spanMode);
-            foreach(fileName; files) {
-                if (fileName.isFile() && (extMatch(fileName, defaultExts) ||
-                                          nameMatch(fileName, defaultNames))) {
-                    fileList ~= fileName;
+        try {
+            if (item == "-") {
+                fileList ~= item;
+            } else if (item.isFile) {
+                fileList ~= item;
+            } else if (item.isDir) {
+                auto dirName = buildNormalizedPath(item);
+                auto files = dirEntries(dirName, spanMode, follow);
+                foreach(fileName; files) {
+                    if (fileName.isFile() && (extMatch(fileName, defaultExts) ||
+                                              nameMatch(fileName, defaultNames))) {
+                        fileList ~= fileName;
+                    }
                 }
             }
+        } catch(std.file.FileException e) {
+            writeln("Unknown file: ", item);
+            return -1;
         }
     }
 
     if (arguments["--sort-files"].isTrue) {
-        sort(fileList);
+        std.algorithm.sort(fileList);
     }
 
     if (arguments["-f"].isTrue) {
@@ -257,6 +295,7 @@ File type options:
 
         if (filename == "-") {
             inp = new EndianStream(std.cstream.din);
+            bom = -1;
         } else {
             fstream = new BufferedFile(filename);
             inp = new EndianStream(fstream);
